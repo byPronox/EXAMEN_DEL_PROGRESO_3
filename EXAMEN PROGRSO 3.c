03d465fa4ee2fe40d966c4557ab2315f5192c7f5
Laboratorio <laboratorio@udla-ec.int>
2023-07-10 16:09:58 -0500

    Final del programa con los respectivos comentarios

--- EXAMEN PROGRSO 3.c
+++ EXAMEN PROGRSO 3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>//Creacion de las 3 librerias necesarias para el codigo
 
-typedef struct {
+typedef struct {//Creacion del struct para almacenar cada estudiante con susu datos
     int numero;
     char nombre[40];
     char carrera[40];
@@ -10,7 +10,7 @@ typedef struct {
     float promedio;
 } Estudiante;
 
-void mayormenorestudiante(Estudiante* students, int numEstudiantes) {
+void mayormenorestudiante(Estudiante* students, int numEstudiantes) {//Creacion del void para el promedio mayor de los estudiantes con su respectivo for y if
     int i, j;
     Estudiante temp;
 
@@ -25,8 +25,8 @@ void mayormenorestudiante(Estudiante* students, int numEstudiantes) {
     }
 }
 
-void calculapromedio(Estudiante* students, int numEstudiantes) {
-    for (int i = 0; i < numEstudiantes; i++) {
+void calculapromedio(Estudiante* students, int numEstudiantes) {//Void para calcular el promedio y se almacena dentro de calculapromedio
+    for (int i = 0; i < numEstudiantes; i++) {//Con doble for 
         float sum = 0;
         for (int j = 0; j < 3; j++) {
             sum += students[i].notas[j];
@@ -38,8 +38,8 @@ void calculapromedio(Estudiante* students, int numEstudiantes) {
 void mayormenorestudiante(Estudiante* students, int numEstudiantes);
 void calculapromedio(Estudiante* students, int numEstudiantes);
 
-int main() {
-    FILE* inputFile = fopen("alumnos.txt", "r");
+int main() {//Creacion del int main para ejecutar lo anterior
+    FILE* inputFile = fopen("alumnos.txt", "r");//Abrimos el archivo dado
     if (inputFile == NULL) {
         printf("No se pudo abrir el archivo de entrada.\n");
         return 1;
@@ -48,27 +48,27 @@ int main() {
     int numEstudiantes = 0;
     Estudiante students[50];
 
-    // Leer los datos de los estudiantes desde el archivo de entrada
+    // Leer los datos de los estudiantes desde el archivo de entrada con while hasta que los lea todos los 5
     while (fscanf(inputFile, "%d;%[^;];%[^;];%f;%f;%f", &students[numEstudiantes].numero, students[numEstudiantes].nombre, students[numEstudiantes].carrera, &students[numEstudiantes].notas[0], &students[numEstudiantes].notas[1], &students[numEstudiantes].notas[2]) == 6) {
         numEstudiantes++;
     }
 
     fclose(inputFile);
 
-    // Calcular el promedio de cada estudiante
+    // Calcular el promedio de cada estudiante con las 3 notas
     calculapromedio(students, numEstudiantes);
 
-    // Ordenar los estudiantes por promedio de mayor a menor
+    // Ordenar los estudiantes por promedio de mayor a menor promedio con la ayuda del anterior void
     mayormenorestudiante(students, numEstudiantes);
 
-    // Crear el archivo de salida ordenado
+    // Creacion de el archivo de salida ordenado llamado alumnos_ordenados
     FILE* outputFile = fopen("alumnos_ordenados.txt", "w");
     if (outputFile == NULL) {
         printf("No se pudo crear el archivo de salida.\n");
         return 1;
     }
 
-    for (int i = 0; i < numEstudiantes; i++) {
+    for (int i = 0; i < numEstudiantes; i++) {//Se imprime cada estudiante con un for hasta llegar a los 5
         fprintf(outputFile, "%d;%s;%s;%.2f;%.2f;%.2f;%.2f\n", students[i].numero, students[i].nombre, students[i].carrera, students[i].notas[0], students[i].notas[1], students[i].notas[2], students[i].promedio);
     }
 
